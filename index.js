961f09b2536dff6e1974b462af9c7dca8c966e95
jamescross <crossjames813@gmail.com>
2021-08-28 21:34:21 -0500

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,67 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+let employee 
+ function createEmployeeRecord(arr){
+    employee = {
+        firstName: arr[0], 
+        familyName: arr[1], 
+        title: arr[2], 
+        payPerHour: arr[3], 
+        timeInEvents: [], 
+        timeOutEvents: []}
+    
+return employee
+}
+
+function createEmployeeRecords(arr){
+    let employees = []
+    for(let i = 0; i < arr.length; i++){
+       employees.push(createEmployeeRecord(arr[i]))
+    }
+    return employees
+}
+
+let createTimeInEvent = function(dateStamp){
+     this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(dateStamp.slice(11), 10),
+        date: dateStamp.slice(0, 10),
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp){
+    this.timeOutEvents.push({
+       type: "TimeOut",
+       hour: parseInt(dateStamp.slice(11), 10),
+       date: dateStamp.slice(0, 10),
+   })
+   return this
+}
+
+function hoursWorkedOnDate(date){
+    let timeInDate = this.timeInEvents.find(e => e.date === date).hour
+    let timeOutDate = this.timeOutEvents.find(e => e.date === date).hour
+    let hoursWorked = (timeOutDate - timeInDate)/100
+    return hoursWorked 
+}
+
+function wagesEarnedOnDate(date){
+    let wage = this.payPerHour
+    let hoursWorked = hoursWorkedOnDate.call(employee, date)
+    return wage * hoursWorked
+}
+
+function calculatePayroll(arr){
+    let employeePay = arr.map((employee) =>
+    {return allWagesFor(employee)})
+    return employeePay.reduce((x, y)=> x + y)
+}
+
+function findEmployeeByFirstName(arr, name){
+    return arr.find(e => e.firstName === name)
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
@@ -20,3 +81,4 @@ let allWagesFor = function () {
 
     return payable
 }
+
